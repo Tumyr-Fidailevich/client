9bfecfdfaf6aaa33a715603d9a5fcc3562dd99f7
Timur <timurkashapov15@gmail.com>
2024-02-01 00:35:08 +0300

    update pch

--- include/pch.h
+++ include/pch.h
@@ -2,12 +2,14 @@
 #define PCH_H
 
 #include <iostream>
+#include <string>
 #include <cctype>
 #include <algorithm>
 #include <numeric>
 #include <mutex>
 #include <thread>
 #include <condition_variable>
+#include "boost/bind/bind.hpp"
 #include "boost/asio.hpp"
 
 #endif //PCH_H
