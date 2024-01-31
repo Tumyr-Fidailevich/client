186b941675fa2d8b11dff3e54bf2445db5da7ddd
Timur <timurkashapov15@gmail.com>
2024-02-01 00:36:49 +0300

    add try except to main cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <csignal>
 #include "data_processor.h"
 #include "shared_buffer.h"
 #include "input_listener.h"
@@ -10,21 +11,34 @@ const std::string IP_ADDRESS = "127.0.0.1";
 
 const short PORT = 1234;
 
+void signalHandler(int signum) {
+    std::cout << "Shutdown client" << std::endl;
+    exit(signum);
+}
+
 int main(int argc, char* argv[])
 {
-    boost::asio::io_service ioService;
-    tcp::socket socket(ioService);
-    socket.connect(tcp::endpoint(address::from_string(IP_ADDRESS), PORT));
-
-    
+    setlocale(LC_ALL, "RU");
     SharedBuffer buffer;
     SyncOstream ostream(std::cout);
-    InputListener listener(buffer, ostream);
-    DataProcessor dataProcessor(buffer, socket, ostream);
 
-    std::thread inputThread(&InputListener::run, listener);
-    std::thread processingThread(&DataProcessor::run, dataProcessor);
+    try
+    {
+        boost::asio::io_context ioContext;
+    
+        InputListener listener(buffer, ostream);
+        DataProcessor dataProcessor(buffer, ioContext, tcp::endpoint(address::from_string(IP_ADDRESS), PORT), ostream);
 
-    inputThread.join();
-    processingThread.join();
+        std::thread inputThread(&InputListener::run, &listener);
+        std::thread processingThread(&DataProcessor::run, &dataProcessor);
+
+        std::signal(SIGINT, signalHandler);
+
+        inputThread.join();
+        processingThread.join();
+    }
+    catch(const std::exception& e)
+    {
+        ostream << "Error during starting client" << std::endl;
+    }
 }
